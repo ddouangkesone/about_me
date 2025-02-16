7453c9be3d324a774cc938cbca8177db83be15c3
Daniel Douangkesone <karateporkchop@gmail.com>
2025-02-16 12:44:18 -0500

    stashing work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react';
-// import styled from 'styled-components'
-import ScrollTrigger from 'react-scroll-trigger';
-import Percentage from './components/Percentage';
-import Modal from './components/Modal';
-import SocialMedia from './components/social_media'
+import { useEffect, useState } from 'react';
 import DanielDouangkesone from './components/daniel_douangkesone';
+import Modal from './components/Modal';
+import Percentage from './components/Percentage';
 import RecentWorkThumbnails from './components/recent_work_thumbnails';
+import ScrollTrigger from 'react-scroll-trigger';
+import SocialMedia from './components/social_media'
 
-export default function App() {
+export default function App(props) {
   const [state, setState] = useState({
     showNavBar: false,
     introVisible: true,
@@ -32,12 +31,6 @@ export default function App() {
 
   function handleModal(modalImage) {
     setState(prevState => ({ ...prevState, modalOpen: !modalOpen, modalImage }))
-  }
-
-  function escFunction(event) {
-    if (event.keyCode === 27) {
-      setState(prevState => ({ ...prevState, modalOpen: false }))
-    }
   }
 
   const recentWork = sections.map((element, index) => {
@@ -82,105 +75,103 @@ export default function App() {
     }, 1000)
   }, [])
 
-  return (
-    <div id="app">
+  return (<div id="app">
 
-      {/* Modal */}
-      {modalOpen ? <Modal image={modalImage} handleModal={handleModal} /> : null}
+    {/* Modal */}
+    {modalOpen ? <Modal image={modalImage} handleModal={handleModal} /> : null}
 
-      {/* Background */}
-      <div className="background-landscape" />
+    {/* Background */}
+    <div className="background-landscape" />
 
-      {/* Welcome Message */}
-      {!removeIntro ? <div className={`fixed top-0 h-100 w-100 flex items-center ph5 white ${introVisible ? 'fade-in' : 'fade-out'}`}>
-        <div className="dtc v-mid tc white ph3 ph4-l w-100 pacifico">
-          <h1>
-            Welcome to my portfolio site
-          </h1>
-          <p className="josefin">
-            Scroll down for more content
-          </p>
-        </div>
-      </div> : null}
+    {/* Welcome Message */}
+    {!removeIntro ? <div className={`fixed top-0 h-100 w-100 flex items-center ph5 white ${introVisible ? 'fade-in' : 'fade-out'}`}>
+      <div className="dtc v-mid tc white ph3 ph4-l w-100 pacifico">
+        <h1>
+          Welcome to my portfolio site
+        </h1>
+        <p className="josefin">
+          Scroll down for more content
+        </p>
+      </div>
+    </div> : null}
 
-      {/* Top Nav Bar */}
-      {
-        showNavBar ?
-          <div
-            className={`pacifico ph5-l ph3 z-1 fixed top-0 w-100 tc name pt3 ${navShadow ? 'shadow-3' : ''} black bg-washed-green ${(introVisible) ? 'fade-out' : 'nav-fade-in'}`}
-          >
-            <h2 className="mb3 mt0 f-subheadline-l f1-m f2">
-              {name}
-            </h2>
-            <SocialMedia content={social_media} />
-          </div> : null
-      }
+    {/* Top Nav Bar */}
+    {
+      showNavBar ?
+        <div
+          className={`pacifico ph5-l ph3 z-1 fixed top-0 w-100 tc name pt3 ${navShadow ? 'shadow-3' : ''} black bg-washed-green ${(introVisible) ? 'fade-out' : 'nav-fade-in'}`}
+        >
+          <h2 className="mb3 mt0 f-subheadline-l f1-m f2">
+            {name}
+          </h2>
+          <SocialMedia content={social_media} />
+        </div> : null
+    }
 
-      {/* Spacer */}
-      <div className="w-100 w-50-l vh-100 spacer"></div>
+    {/* Spacer */}
+    <div className="w-100 w-50-l vh-100 spacer"></div>
 
-      {/* Main Content */}
-      <div className="w-100 w-50-l fr tc ph4 washed-green-o2">
+    {/* Main Content */}
+    <div className="w-100 w-50-l fr tc ph4 washed-green-o2">
 
-        {/* About Me */}
-        <div className="hello vh-75 flex flex-column justify-center f4">
-          <h1 className="josefin mb4">About Me</h1>
-          <span>
-            I'm a front end dev based out of New York City. I started taking freelance design jobs and
-            naturally progressed into web development. Making beautiful smart websites is my pride and passion.
-          </span>
-        </div>
-
-        {/* Recent Work */}
-
-        {/* This ScrollTrigger removes the the intro text */}
-        <ScrollTrigger
-          onEnter={() => {
-            setState(prevState => ({ ...prevState, removeIntro: true })
-            )
-          }
-          }
-        />
-        {recentWork}
-
-        {/* Skills */}
-        <div className="skills flex flex-column vh-75 flex flex-column justify-center f4">
-          <div className="w-100 josefin mb4">
-            <h1>Related Skills</h1>
-          </div>
-          {skillslist}
-        </div>
+      {/* About Me */}
+      <div className="hello vh-75 flex flex-column justify-center f4">
+        <h1 className="josefin mb4">About Me</h1>
+        <span>
+          I'm a front end dev based out of New York City. I started taking freelance design jobs and
+          naturally progressed into web development. Making beautiful smart websites is my pride and passion.
+        </span>
       </div>
 
-      {/* Footer */}
-      <footer className="w-100 pv4 tc fr bg-washed-green vh-100 relative">
-        <div className="footer-top w-100 relative">
-          <div className="absolute bottom-0 flex w-100 h-75 pt4">
-            <div className="w-50 h-100 flex items-center justify-center pv5 pacifico ph3">
-              <h2> Thanks for your time and interest!! </h2>
-            </div>
-            <div className="w-50 h-100 flex flex-column justify-center bl pv5 pl4 tl">
-              <div>
-                <h3 className="mb0">Get in touch</h3>
-                <br />
-                <a href="mailto:ddouangkesone@gmail.com">Send me an email</a>
-                <br />
-                <a href="https://docs.google.com/document/d/1K3PVDp56NUnAAbeOxBvs3NfLMDc8aZNaMvqoTF3zq1M/edit">Check out my resume</a>
-              </div>
-              <div className="mt5">
-                <h3>Find me</h3>
-                <SocialMedia content={social_media} />
-              </div>
-            </div>
+      {/* Recent Work */}
+
+      {/* This ScrollTrigger removes the the intro text */}
+      <ScrollTrigger
+        onEnter={() => {
+          setState(prevState => ({ ...prevState, removeIntro: true })
+          )
+        }
+        }
+      />
+      {recentWork}
+
+      {/* Skills */}
+      <div className="skills flex flex-column vh-75 flex flex-column justify-center f4">
+        <div className="w-100 josefin mb4">
+          <h1>Related Skills</h1>
+        </div>
+        {skillslist}
+      </div>
+    </div>
+
+    {/* Footer */}
+    <footer className="w-100 pv4 tc fr bg-washed-green vh-100 relative">
+      <div className="footer-top w-100 relative">
+        <div className="absolute bottom-0 flex w-100 h-75 pt4">
+          <div className="w-50 h-100 flex items-center justify-center pv5 pacifico ph3">
+            <h2> Thanks for your time and interest!! </h2>
           </div>
-          <div className="footer-bottom w-100">
-            <ScrollTrigger
-              onEnter={() => { setState(prevState => ({ ...prevState, navShadow: false })) }}
-              onExit={() => { setState(prevState => ({ ...prevState, navShadow: true })) }}
-            />
+          <div className="w-50 h-100 flex flex-column justify-center bl pv5 pl4 tl">
+            <div>
+              <h3 className="mb0">Get in touch</h3>
+              <br />
+              <a href="mailto:ddouangkesone@gmail.com">Send me an email</a>
+              <br />
+              <a href="https://docs.google.com/document/d/1K3PVDp56NUnAAbeOxBvs3NfLMDc8aZNaMvqoTF3zq1M/edit">Check out my resume</a>
+            </div>
+            <div className="mt5">
+              <h3>Find me</h3>
+              <SocialMedia content={social_media} />
+            </div>
           </div>
         </div>
-      </footer>
-    </div>
-  )
+        <div className="footer-bottom w-100">
+          <ScrollTrigger
+            onEnter={() => { setState(prevState => ({ ...prevState, navShadow: false })) }}
+            onExit={() => { setState(prevState => ({ ...prevState, navShadow: true })) }}
+          />
+        </div>
+      </div>
+    </footer>
+  </div>)
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router";
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
